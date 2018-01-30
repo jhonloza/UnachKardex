1711cdf90239c804fb67e1a257dd0991a4eaff9f
Jhon <>
2018-01-29 19:12:37 -0500

    Vistas

--- UnachKardex/src/unachkardex/vistas/FormCliente.java
+++ UnachKardex/src/unachkardex/vistas/FormCliente.java
@@ -1,36 +1,47 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package unachkardex.vistas;
 
+import java.awt.*;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.*;
 import javafx.stage.Stage;
+import javafx.geometry.*;
+import java.util.*;
+import javafx.scene.Node;
+import javafx.scene.text.Text;
 
 public class FormCliente extends Application {
+
+    private HBox panelInterno1;
+    private Text txtCedula;
+    Label campo1;
+    private VBox temporal;
     
     @Override
     public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
         
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
+        campo1 = new Label("Cedula");
+        //LABELS DE LOS CAMPOS A USAR
+        txtCedula = new Text("Cedula");
+        txtCedula.setFont(javafx.scene.text.Font.font("Times New Roman", 25));
+//        txtCedula.setFill(Color.RED);
+//        labelNombres = new Label("Nombres");
+//        labelApellidos = new Label("Apellidos");
+//        labelFecha = new Label("Fecha de Nacimiento");
+//        labelDir = new Label("Direccion");
+//        labelTelf = new Label("Telefono");
+//        labelEmail = new Label("E-mail");
+
+        //PANEL INTERNO CON CAMPOS A USARSE
+        panelInterno1=new HBox();
         
-        Scene scene = new Scene(root, 300, 250);
+        panelInterno1.getChildren().add(new Text("Cedula"));
+//        
+        
+        Scene scene = new Scene(panelInterno1);
         
         primaryStage.setTitle("Hello World!");
         primaryStage.setScene(scene);
@@ -40,5 +51,4 @@ public class FormCliente extends Application {
     public static void main(String[] args) {
         launch(args);
     }
-    
 }
