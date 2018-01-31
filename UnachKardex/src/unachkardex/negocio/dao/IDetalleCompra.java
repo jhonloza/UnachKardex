4aae2dcdcc74cd5dac6990c63478790737990be5
Davidssito Campos <Davidssito Campos@DESKTOP-S8KPSSD>
2018-01-30 23:13:48 -0500

    David Campos IDC

--- UnachKardex/src/unachkardex/negocio/dao/IDetalleCompra.java
+++ UnachKardex/src/unachkardex/negocio/dao/IDetalleCompra.java
@@ -9,9 +9,7 @@ import java.util.ArrayList;
 public interface IDetalleCompra {
     
     public int ingresar(DetalleCompra detalleCompra) throws Exception;
-    
-    public int modificar(DetalleCompra detalleCompra) throws Exception;
-    
+   public int modificar(DetalleCompra detalleCompra) throws Exception;
     public int eliminar(DetalleCompra detalleCompra) throws Exception;
     
     public DetalleCompra obtener(int codDetalleCompra) throws Exception;
