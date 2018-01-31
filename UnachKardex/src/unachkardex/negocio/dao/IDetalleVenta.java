0a4fd049334cccf968a11d136df90215fe9946cd
Davidssito Campos <Davidssito Campos@DESKTOP-S8KPSSD>
2018-01-30 23:13:27 -0500

    David Campos IDT

--- UnachKardex/src/unachkardex/negocio/dao/IDetalleVenta.java
+++ UnachKardex/src/unachkardex/negocio/dao/IDetalleVenta.java
@@ -8,13 +8,10 @@ import unachkardex.negocio.entidades.DetalleVenta;
 public interface IDetalleVenta {
     
     public int ingresar(DetalleVenta detalleVenta) throws Exception;
-    
     public int modificar(DetalleVenta detalleVenta) throws Exception;
-    
     public int eliminar(DetalleVenta detalleVenta) throws Exception;
     
     public DetalleVenta obtener(int codDetalleVenta) throws Exception;
-    
     public ArrayList<DetalleVenta> obtener() throws Exception;
     
 }
