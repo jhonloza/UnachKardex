d7eb81520f3cae4374b0060e01e155f73b1056a7
JhonPC <JhonPC@DESKTOP-T9J7GH5>
2018-02-04 21:58:27 -0500

    vista proveedor

--- UnachKardex/src/unachkardex/vistas/FormProveedor.java
+++ UnachKardex/src/unachkardex/vistas/FormProveedor.java
@@ -1,3 +1,4 @@
+
 package unachkardex.vistas;
 
 import com.sun.javafx.geom.Area;
@@ -21,28 +22,27 @@ import javafx.event.Event;
 import javafx.event.EventDispatchChain;
 import javafx.event.EventDispatcher;
 import javafx.scene.Node;
-import javafx.scene.text.Text;
+import javafx.scene.text.*;
 import unachkardex.negocio.dao.*;
 import unachkardex.negocio.entidades.*;
 import unachkardex.negocio.impl.*;
 
-
 public class FormProveedor extends Application {
-    
-    private Text txtRuc;
-    private Text txtNombre;
-    private Text txtDireccion;
-    private Text txtTelefono;
-    private Text txtEmail;
+    private Label titulo;
+    private Label txtRuc;
+    private Label txtNombre;
+    private Label txtDireccion;
+    private Label txtTelefono;
+    private Label txtEmail;
 
     Image logo;
     ImageView visorlogo;
 
-    private TextArea ruc;
-    private TextArea nombre;
-    private TextArea direccion;
-    private TextArea telefono;
-    private TextArea email;
+    private TextField ruc;
+    private TextField nombre;
+    private TextField direccion;
+    private TextField telefono;
+    private TextField email;
 
     private Button btnIngresar;
     private Button btnEliminar;
@@ -50,57 +50,66 @@ public class FormProveedor extends Application {
     private Button btnLimpiar;
     private Button btnCancelar;
 
-    private HBox pnlRuc;
-    private GridPane pnlNombDir;
-    private GridPane pnlContactos;
-   
+    private VBox pnlRucNom;
+    private VBox pnlRN;
+    private VBox pnlTitulo;
+    private HBox pnlIcInfo;
+    private VBox pnlTItProv;
+    private VBox pnlIItProv;
+    private HBox pnlSup;
+    private HBox pnlInt;
     private HBox pnlBotones;
     private VBox pntPrincipal;
 
     @Override
     public void start(Stage primaryStage) {
-
-//LABELS DE LOS CAMPOS A USAR
-        txtRuc = new Text("RUC:      ");
-        txtRuc.setFont(Font.font("Broadway", 25));
-        txtRuc.setFill(Color.BLUEVIOLET);
-        
-        txtNombre = new Text("Nombres:");
-        txtNombre.setFont(Font.font("Broadway", 20));
-        txtNombre.setFill(Color.BLUEVIOLET);
-        
-        txtDireccion = new Text("Direccion:");
-        txtDireccion.setFont(Font.font("Broadway", 20));
-        txtDireccion.setFill(Color.BLUEVIOLET);
-        
-        txtTelefono = new Text("Telefono:");
-        txtTelefono.setFont(Font.font("Broadway", 20));
-        txtTelefono.setFill(Color.BLUEVIOLET);
-        txtEmail = new Text("E-mail:");
-        txtEmail.setFont(Font.font("Broadway", 20));
-        txtEmail.setFill(Color.BLUEVIOLET);
-        
-        
+        //Sup izq
+        titulo=new Label("\" Proveedor \"");
+        titulo.setFont(Font.font("Berlin Sans FB Demi", 20));
+        txtRuc = new Label("Ruc:");
+        txtRuc.setFont(Font.font("Berlin Sans FB Demi", 20));
+        txtNombre = new Label("Nombres:");
+        txtNombre.setFont(Font.font("Berlin Sans FB Demi", 20));
+        pnlRucNom=new VBox(5);
+        pnlRucNom.getChildren().addAll(txtRuc,txtNombre);
+        pnlRucNom.setAlignment(Pos.CENTER_RIGHT);
+        txtDireccion = new Label("Direccion:");
+        txtDireccion.setFont(Font.font("Berlin Sans FB Demi", 20));
+        txtTelefono = new Label("Telefono:");
+        txtTelefono.setFont(Font.font("Berlin Sans FB Demi", 20));
+        txtEmail = new Label("E-mail:");
+        txtEmail.setFont(Font.font("Berlin Sans FB Demi", 20));
+        pnlTItProv=new VBox(10);
+        pnlTItProv.getChildren().addAll(txtDireccion,txtTelefono,txtEmail);
+        pnlTItProv.setAlignment(Pos.CENTER_RIGHT);
         //CAJAS DE TEXTO PARA CAMPOS
-        ruc = new TextArea("");
-        nombre = new TextArea("");
-        direccion = new TextArea("");
-        telefono = new TextArea("");
-        email = new TextArea("");
-        
+        ruc = new TextField("");
+        nombre = new TextField("");
+        pnlRN=new VBox(10);
+        pnlRN.getChildren().addAll(ruc,nombre);
+        pnlRN.setAlignment(Pos.CENTER);
+        direccion = new TextField("");
+        telefono = new TextField("");
+        email = new TextField("");
+        pnlIItProv=new VBox(10);
+        pnlIItProv.getChildren().addAll(direccion,telefono,email);
+        pnlIItProv.setAlignment(Pos.CENTER);
+        //LOGO
+        logo=new Image("file:src\\unachkardex\\multimedia\\iconoProveedor.png");
+        visorlogo=new ImageView(logo);
+        visorlogo.setFitHeight(100);
+        visorlogo.setFitWidth(100);
         //BOTONES A USAR
         btnIngresar = new Button("Ingresar");
-        btnIngresar.setFont(Font.font("ALGERIAN",FontWeight.BLACK, 25));
+        btnIngresar.setFont(Font.font("Berlin Sans FB Demi", 15));
         btnIngresar.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
                 btnIngresarEventHandler(event);
             }
         });
-        
-        
         btnLimpiar = new Button("Limpiar");
-        btnLimpiar.setFont(Font.font("ALGERIAN",FontWeight.BOLD, 25));
+        btnLimpiar.setFont(Font.font("Berlin Sans FB Demi", 15));
         btnLimpiar.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
@@ -108,86 +117,58 @@ public class FormProveedor extends Application {
             }
         });
         btnCancelar = new Button("Cancelar");
-        btnCancelar.setFont(Font.font("ALGERIAN",FontWeight.BOLD, 25));
-//INGRESO EN PANELES
-//SUPERIOR
-        pnlRuc = new HBox(20);
-        pnlRuc.getChildren().addAll(txtRuc, ruc);
-//nombre
-        pnlNombDir = new GridPane();
-        pnlNombDir.setHgap(20);
-        pnlNombDir.setVgap(20);
-        pnlNombDir.add(txtNombre, 0, 0);
-        pnlNombDir.add(nombre, 1, 0);
-        pnlNombDir.add(txtDireccion, 2, 0);
-        pnlNombDir.add(direccion, 3, 0);
-//Contactos
-        pnlContactos = new GridPane();
-        pnlContactos.setHgap(20);
-        pnlContactos.setVgap(10);
-        pnlContactos.add(txtTelefono, 0, 0);
-        pnlContactos.add(telefono, 1, 0);
-        pnlContactos.add(txtEmail, 0, 1);
-        pnlContactos.add(email, 1, 1);
-
-    
-//botones
-        pnlBotones = new HBox(20);
-        pnlBotones.getChildren().addAll(btnIngresar, btnLimpiar, btnCancelar);
+        btnCancelar.setFont(Font.font("Berlin Sans FB Demi", 15));
+        pnlIcInfo=new HBox(10);
+        pnlIcInfo.getChildren().addAll(pnlRucNom,pnlRN);
+        pnlIcInfo.setAlignment(Pos.CENTER);
+        pnlTitulo=new VBox(10);
+        pnlTitulo.getChildren().addAll(titulo,pnlIcInfo);
+        pnlTitulo.setAlignment(Pos.TOP_CENTER);
+        pnlSup=new HBox(10);
+        pnlSup.getChildren().addAll(pnlTitulo,visorlogo);
+        pnlSup.setPadding(new Insets(10));
+        pnlInt=new HBox(10);
+        pnlInt.getChildren().addAll(pnlTItProv,pnlIItProv);
+        pnlInt.setPadding(new Insets(10));
+        pnlBotones=new HBox(10);
+        pnlBotones.getChildren().addAll(btnIngresar,btnLimpiar,btnCancelar);
         pnlBotones.setAlignment(Pos.CENTER);
-        //final
-        pntPrincipal = new VBox(10);
-//        btnEliminar=new Button("Temporal");
-//        btnEliminar.setFont(Font.font("Arial Black",40));
-        logo = new Image("file:src\\unachkardex\\multimedia\\proveedorr.jpg");
-        visorlogo = new ImageView();
-        visorlogo.setImage(logo);
-        visorlogo.setFitHeight(200);
-        visorlogo.setPreserveRatio(true);
-        pntPrincipal.getChildren().addAll(visorlogo, pnlRuc, pnlNombDir, pnlContactos, pnlBotones);
-        pntPrincipal.setAlignment(Pos.CENTER);
-        pntPrincipal.setPadding(new Insets(25));
-        
-        
-        Scene scene = new Scene(pntPrincipal, 600, 500);
-
-        primaryStage.setTitle("Proveedor.");
+        pnlBotones.setPadding(new Insets(10));
+        pntPrincipal=new VBox(10);
+        pntPrincipal.getChildren().addAll(pnlSup,pnlInt,pnlBotones);
+        pntPrincipal.setPadding(new Insets(10));
+        Scene scene = new Scene(pntPrincipal, 400, 320);
+        primaryStage.setTitle("Proveedor");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
-
     public static void main(String[] args) {
         launch(args);
     }
-    public void btnLimpiarEventHandler(ActionEvent event){
-       
-            ruc.setText(null);
-            nombre.setText("");
-            direccion.setText(null);
-            telefono.setText("");
-            email.setText(null);
-        }
-            
-   
-    public void btnIngresarEventHandler(ActionEvent event){
-        IProveedor proveedorDao=new ImplProveedor();
+    public void btnLimpiarEventHandler(ActionEvent event) {
+        ruc.setText("");
+        nombre.setText("");
+        direccion.setText("");
+        telefono.setText("");
+        email.setText("");
+    }
+    public void btnIngresarEventHandler(ActionEvent event) {
+        IProveedor proveedorDao = new ImplProveedor();
         try {
-            Proveedor nuevoProveedor=new Proveedor();
+            Proveedor nuevoProveedor = new Proveedor();
             nuevoProveedor.setRuc(ruc.getText());
             nuevoProveedor.setNombre(nombre.getText());
             nuevoProveedor.setDireccion(direccion.getText());
             nuevoProveedor.setTelefono(telefono.getText());
             nuevoProveedor.seteMail(email.getText());
-            
-            if(proveedorDao.ingresar(nuevoProveedor)>0){
+
+            if (proveedorDao.ingresar(nuevoProveedor) > 0) {
                 System.out.println("Ingreso Correcto..");
-            }
-            else{
+            } else {
                 System.out.println("Error de Ingreso..");
             }
         } catch (Exception e) {
-            System.out.println("Error de Ingreso"+e.getMessage());
+            System.out.println("Error de Ingreso" + e.getMessage());
         }
     }
-
 }
