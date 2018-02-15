0158646ca1d7dd2fd9c3c8f41f2ca1e119364f02
Jhon <Jhon@DESKTOP-CS9FJVL>
2018-02-15 15:50:53 -0500

    detalle Venta

--- UnachKardex/src/unachkardex/vistas/FormNDetalleVenta.java
+++ UnachKardex/src/unachkardex/vistas/FormNDetalleVenta.java
@@ -22,81 +22,78 @@ import unachkardex.accesodatos.*;
 
 public class FormNDetalleVenta extends Application {
 
+    private Label codigo;
+    private Label nombre;
+    private Label cantidad;
+    private Label precioU;
+    private Label precioT;
+    private ObservableList<TextField> lstCodigo=FXCollections.observableArrayList();
+    private ArrayList<TextField> listaCodigo;
+    private ObservableList<TextField> lstNombre=FXCollections.observableArrayList();
+    private ObservableList<TextField> lstCantidad=FXCollections.observableArrayList();
+    private ObservableList<TextField> lstPrecioU=FXCollections.observableArrayList();
+    private ObservableList<TextField> lstPrecioT=FXCollections.observableArrayList();
     private Image pFondo;
     private BackgroundImage fondo;
-    private Label txtCodigoDV;
-    private Label txtProducto;
-    private Label txtfacturaV;
-
-    private Label txtCantidad;
-    private Label txtPrecioTotal;
-
-    private TextField codigodv;
-    private TextField producto;
-    private TextField facturav;
-
-    private TextField cantidad;
-    private TextField preciototal;
-
-    private Button btnIngresar;
-    private Button btnLimpiar;
-
-    private HBox pnlced;
-    private GridPane pnlNombApe;
-    private GridPane pnlFechDir;
-   
-    private HBox pnlBotones;
+    private HBox pnlItems;
+    private HBox items;
     private VBox pntPrincipal;
 
     @Override
 
     public void start(Stage primaryStage) {
-        //LABELS DE LOS CAMPOS A USAR
-        txtCodigoDV = new Label("CODIGO DETALLE VENTA");
-        txtProducto = new Label("PRODUCTO");
-        txtfacturaV = new Label("FACTURA VENTA");
-        txtCantidad = new Label("CANTIDAD");
-        txtPrecioTotal = new Label("PRECIO TOTAL");
-        codigodv = new TextField("");
-        producto = new TextField("");
-        facturav = new TextField("");
-        cantidad = new TextField("");
-        preciototal = new TextField("");
-        /////////////////////////////////////
-        btnIngresar = new Button("Ingresar");
-        btnIngresar.setFont(Font.font("Arial Black", 20));
-        btnLimpiar = new Button("Limpiar");
-        btnLimpiar.setFont(Font.font("Arial Black", 20));
-        //////////////////////////////////
-        pnlced = new HBox(20);
-        pnlced.getChildren().addAll(txtCodigoDV, codigodv);
-        pnlNombApe = new GridPane();
-        pnlNombApe.setHgap(10);
-        pnlNombApe.setVgap(10);
-        pnlNombApe.add(txtProducto, 0, 0);
-        pnlNombApe.add(producto, 1, 0);
-        pnlNombApe.add(txtfacturaV, 2, 0);
-        pnlNombApe.add(facturav, 3, 0);
-        pnlNombApe.setAlignment(Pos.CENTER);
-        ////////////////////////////
-        pnlFechDir=new GridPane();
-        pnlFechDir.setHgap(20);
-        pnlFechDir.setVgap(10);
-        pnlFechDir.add(txtCantidad, 0, 0);
-        pnlFechDir.add(cantidad, 1, 0);
-        pnlFechDir.add(txtPrecioTotal, 0, 1);
-        pnlFechDir.add(preciototal, 1, 1);
-        pnlBotones=new HBox(20);
-        pnlBotones.getChildren().addAll(btnIngresar,btnLimpiar);
-        pnlBotones.setAlignment(Pos.CENTER);
-        pntPrincipal=new VBox(10);
-        pntPrincipal.getChildren().addAll(pnlced,pnlNombApe,pnlFechDir,pnlBotones);
-        pntPrincipal.setAlignment(Pos.CENTER);
-        pntPrincipal.setPadding(new Insets(10));
+        //barra Items
+        codigo = new Label("Codigo");
+        codigo.setMaxSize(100, 25);
+        codigo.setMinSize(100, 25);
+        codigo.setStyle("-fx-border-color: mediumblue; -fx-border-width: 2px");
+        codigo.setAlignment(Pos.CENTER);
+        nombre = new Label("Nombre");
+        nombre.setMaxSize(350, 25);
+        nombre.setMinSize(350, 25);
+        nombre.setStyle("-fx-border-color: mediumblue; -fx-border-width: 2px");
+        nombre.setAlignment(Pos.CENTER);
+        cantidad = new Label("Cantidad");
+        cantidad.setMaxSize(100, 25);
+        cantidad.setMinSize(100, 25);
+        cantidad.setStyle("-fx-border-color: mediumblue; -fx-border-width: 2px");
+        cantidad.setAlignment(Pos.CENTER);
+        precioU = new Label("Precio U");
+        precioU.setMaxSize(100, 25);
+        precioU.setMinSize(100, 25);
+        precioU.setStyle("-fx-border-color: mediumblue; -fx-border-width: 2px");
+        precioU.setAlignment(Pos.CENTER);
+        precioT = new Label("Precio T");
+        precioT.setMaxSize(100, 25);
+        precioT.setMinSize(100, 25);
+        precioT.setStyle("-fx-border-color: mediumblue; -fx-border-width: 2px");
+        precioT.setAlignment(Pos.CENTER);
+        pnlItems = new HBox(1);
+        pnlItems.getChildren().addAll(codigo, nombre, cantidad, precioU, precioT);
+        pnlItems.setAlignment(Pos.CENTER);
+        //listado de items
+        listaCodigo=new ArrayList<>();
+        listaCodigo.add(new TextField());
+        lstCodigo.setAll(listaCodigo);
+        items=new HBox();
+        items.getChildren().addAll(lstCodigo);
+        items.setStyle("-fx-border-color: mediumblue; -fx-border-width: 2px");
+        pntPrincipal = new VBox(0);
+        pntPrincipal.getChildren().addAll(pnlItems,items);
         pntPrincipal.setBackground(new Background(fondo));
-        pntPrincipal.setStyle("-fx-padding: 10; -fx-border-color: mediumblue; -fx-border-width: 2px");
+        pntPrincipal.setStyle("-fx-padding: 2; -fx-border-color: mediumblue; -fx-border-width: 2px");
+        Scene scnPrincipal = new Scene(pntPrincipal, 770, 300);
+        primaryStage.setTitle("Sistema de facturacion");
+        primaryStage.setMaximized(false);
+
+        primaryStage.setScene(scnPrincipal);
+        primaryStage.show();
     }
-    
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
     public Node getPantallaP() {
         return pntPrincipal;
     }
