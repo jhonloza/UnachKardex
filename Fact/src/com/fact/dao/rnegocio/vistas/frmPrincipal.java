f61d12456475223ea2a802e723bda20ea545db9e
Joaquin <>
2018-02-16 14:46:11 -0500

    Agregar libreria jfoenix y edicion del formulario principal

--- Fact/src/com/fact/dao/rnegocio/vistas/frmPrincipal.java
+++ Fact/src/com/fact/dao/rnegocio/vistas/frmPrincipal.java
@@ -5,6 +5,7 @@
  */
 package com.fact.dao.rnegocio.vistas;
 
+import com.jfoenix.controls.JFXButton;
 import javafx.application.Application;
 import static javafx.application.Application.launch;
 import javafx.event.ActionEvent;
@@ -15,6 +16,7 @@ import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
@@ -39,39 +41,61 @@ public class frmPrincipal extends Application{
         stage.show();
     }
     public BorderPane root() {
-
         root = new BorderPane();
         root.setTop(menuBar());
-        root.setRight(menuBar());
-        root.setLeft(menuBar());
+//        root.setTop(componentsBar());
+//        root.setLeft(componentsBar());
+//        root.setRight(menuBar());
+//        root.setLeft(menuBar());
         return root;
     }
     
       public MenuBar menuBar() {
+        //MenuBar
         MenuBar MBar = new MenuBar();
-        Menu menuComponentes = new Menu("MENU BAR");
-        MenuItem MIPrueba = new MenuItem("-MENU ITEM");
-        MenuItem MIPrueba2 = new MenuItem("-MENU ITEM 2");
-        MenuItem MIPrueba3 = new MenuItem("-MENU ITEM 3");
-        menuComponentes.getItems().add(MIPrueba);
-        menuComponentes.getItems().add(MIPrueba2);
-        menuComponentes.getItems().add(MIPrueba3);
-        MBar.getMenus().add(menuComponentes);
+        MBar.setStyle("-fx-base:rgb(10,20,25)");
+        //Menu
+        Menu menuCompra = new Menu("Factura");
+        Menu menuVenta = new Menu("Usuarios");
+        Menu menuReportes = new Menu("");
+        //MenuItem
+        MenuItem MIAgregar = new MenuItem("Crear Factura");
+        MenuItem MIEliminar = new MenuItem("-Eliminar");
+        MenuItem MIModificar = new MenuItem(".-Modificar");
+        //Agregar Item al Menu
+        menuCompra.getItems().add(MIAgregar);
+        menuCompra.getItems().add(MIEliminar);
+        menuCompra.getItems().add(MIModificar);
+//        menuVenta.getItems().add(MIAgregar);
+//        menuVenta.getItems().add(MIEliminar);
+//        menuVenta.getItems().add(MIModificar);
+//        menuReportes.getItems().add(MIAgregar);
+//        menuReportes.getItems().add(MIEliminar);
+//        menuReportes.getItems().add(MIModificar);
+
+        //Agregar el Menu al Menu Bar
+        MBar.getMenus().add(menuCompra);
+        MBar.getMenus().add(menuVenta);
+        MBar.getMenus().add(menuReportes);
         return MBar;
     }
 
     public Pane componentsBar() {
-        VBox componentsPanel = new VBox(10);
-        componentsPanel.setStyle("-fx-background-color:rgb(0,92,150)");
-/*      JFXButton btnCliente = new JFXButton("Cliente");
+        HBox componentsPanel = new HBox(10);
+        componentsPanel.setStyle("-fx-background-color:rgb(10,92,50)");
+        JFXButton btnCliente = new JFXButton("Cliente");
+        btnCliente.setStyle("-fx-text-fill:RGB(255,255,255)");
         JFXButton btnEmployee = new JFXButton("Empleado");
-        JFXButton btnFactuta = new JFXButton("Factúra");
+        btnEmployee.setStyle("-fx-text-fill:RGB(255,255,255)");
+        JFXButton btnFactura = new JFXButton("Factúra");
+        btnFactura.setStyle("-fx-text-fill:RGB(255,255,255)");
         JFXButton btnProducto = new JFXButton("Productos");
-         componentsPanel.getChildren().addAll(btnCliente, btnEmployee,btnProducto, btnFactuta);
-        VBox.setVgrow(componentsPanel, Priority.ALWAYS);
+        btnProducto.setStyle("-fx-text-fill:RGB(255,255,255)");
+        componentsPanel.getChildren().addAll(btnCliente, btnEmployee,btnProducto, btnFactura);
+        HBox.setHgrow(componentsPanel, Priority.ALWAYS);
 
-        btnEmployee.setOnAction(RootController.showEmployee(root));
-        btnFactuta.setOnAction(new EventHandler<ActionEvent>() {
+        //btnEmployee.setOnAction(RootController.showEmployee(root));
+        btnFactura.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent t) {
                 Alert alert = new Alert(Alert.AlertType.INFORMATION);
@@ -83,9 +107,8 @@ public class frmPrincipal extends Application{
                 alert.showAndWait();
             }
         });
-        btnCliente.setOnAction(RootController.mostrarCliente(root));
-        btnProducto.setOnAction(RootController.mostrarProducto(root));
-*/
+//        btnCliente.setOnAction(RootController.mostrarCliente(root));
+//        btnProducto.setOnAction(RootController.mostrarProducto(root));
         return componentsPanel;
     }
 
