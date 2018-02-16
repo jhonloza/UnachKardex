48e0eed14052ba5e7a1103669ba3c918b4dbdfd5
David_Campos <David_Campos@192.168.0.109>
2018-02-16 00:07:30 -0500

    David Campos FormEProveedor b

--- UnachKardex/src/unachkardex/vistas/FormEProveedor.java
+++ UnachKardex/src/unachkardex/vistas/FormEProveedor.java
@@ -1,5 +1,5 @@
-package unachkardex.vistas;
 
+package unachkardex.vistas;
 import javafx.application.*;
 import javafx.event.*;
 import javafx.scene.*;
@@ -20,8 +20,7 @@ import unachkardex.negocio.entidades.*;
 import unachkardex.negocio.impl.*;
 import unachkardex.accesodatos.*;
 
-public class FormEProveedor extends Application {
-
+public class FormEProveedor extends Application{
     private Label titulo;
     private Label txtRuc;
     private Label txtNombre;
@@ -35,12 +34,12 @@ public class FormEProveedor extends Application {
     private ImageView visorlogo;
 
     private TextField ruc;
-    private TextField nombre;
-    private TextField direccion;
-    private TextField telefono;
-    private TextField email;
+    private Label nombre;
+    private Label direccion;
+    private Label telefono;
+    private Label email;
 
-    private Button btnIngresar;
+    private Button btnEliminar;
     private Button btnLimpiar;
 
     private VBox pnlRucNom;
@@ -54,9 +53,11 @@ public class FormEProveedor extends Application {
     private HBox pnlBotones;
     private VBox pntPrincipal;
 
+
+
     @Override
-    public void start(Stage primaryStage) {
-        pFondo = new Image("file:src\\unachkardex\\multimedia\\FondoSubVentanas.jpg");
+    public void start(Stage primaryStage) throws Exception {
+         pFondo = new Image("file:src\\unachkardex\\multimedia\\FondoSubVentanas.jpg");
         fondo = new BackgroundImage(pFondo, BackgroundRepeat.REPEAT, BackgroundRepeat.REPEAT, BackgroundPosition.CENTER, BackgroundSize.DEFAULT);
 
         //Sup izq
@@ -80,13 +81,27 @@ public class FormEProveedor extends Application {
         pnlTItProv.setAlignment(Pos.CENTER_RIGHT);
         //CAJAS DE TEXTO PARA CAMPOS
         ruc = new TextField("");
-        nombre = new TextField("");
+        nombre = new Label("");
+        nombre.setMaxSize(150, 25);
+        nombre.setMinSize(150, 25);
+        nombre.setStyle("-fx-border-color: mediumblue; -fx-border-width: 2px");
         pnlRN = new VBox(10);
         pnlRN.getChildren().addAll(ruc, nombre);
         pnlRN.setAlignment(Pos.CENTER);
-        direccion = new TextField("");
-        telefono = new TextField("");
-        email = new TextField("");
+        direccion = new Label("");
+        direccion.setMaxSize(150, 25);
+        direccion.setMinSize(150, 25);
+        direccion.setStyle("-fx-border-color: mediumblue; -fx-border-width: 2px");
+        telefono = new Label("");
+        telefono = new Label("");
+        telefono.setMaxSize(150, 25);
+        telefono.setMinSize(150, 25);
+        telefono.setStyle("-fx-border-color: mediumblue; -fx-border-width: 2px");
+        email = new Label("");
+        email = new Label("");
+        email.setMaxSize(150, 25);
+        email.setMinSize(150, 25);
+        email.setStyle("-fx-border-color: mediumblue; -fx-border-width: 2px");
         pnlIItProv = new VBox(10);
         pnlIItProv.getChildren().addAll(direccion, telefono, email);
         pnlIItProv.setAlignment(Pos.CENTER);
@@ -96,22 +111,22 @@ public class FormEProveedor extends Application {
         visorlogo.setFitHeight(100);
         visorlogo.setFitWidth(100);
         //BOTONES A USAR
-        btnIngresar = new Button("Ingresar");
-        btnIngresar.setFont(Font.font("Berlin Sans FB Demi", 15));
-//        btnIngresar.setOnAction(new EventHandler<ActionEvent>() {
-//            @Override
-//            public void handle(ActionEvent event) {
-//                btnIngresarEventHandler(event);
-//            }
-//        });
+        btnEliminar = new Button("Ingresar");
+        btnEliminar.setFont(Font.font("Berlin Sans FB Demi", 15));
+        btnEliminar.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                btnEliminarEventHandler(event);
+            }
+        });
         btnLimpiar = new Button("Limpiar");
         btnLimpiar.setFont(Font.font("Berlin Sans FB Demi", 15));
-//        btnLimpiar.setOnAction(new EventHandler<ActionEvent>() {
-//            @Override
-//            public void handle(ActionEvent event) {
-//                btnLimpiarEventHandler(event);
-//            }
-//        });
+        btnLimpiar.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                btnLimpiarEventHandler(event);
+            }
+        });
         pnlIcInfo = new HBox(10);
         pnlIcInfo.getChildren().addAll(pnlRucNom, pnlRN);
         pnlIcInfo.setAlignment(Pos.CENTER);
@@ -125,7 +140,7 @@ public class FormEProveedor extends Application {
         pnlInt.getChildren().addAll(pnlTItProv, pnlIItProv);
         pnlInt.setPadding(new Insets(10));
         pnlBotones = new HBox(10);
-        pnlBotones.getChildren().addAll(btnIngresar, btnLimpiar);
+        pnlBotones.getChildren().addAll(btnEliminar, btnLimpiar);
         pnlBotones.setAlignment(Pos.CENTER);
         pnlBotones.setPadding(new Insets(10));
         pntPrincipal = new VBox(10);
@@ -133,17 +148,23 @@ public class FormEProveedor extends Application {
         pntPrincipal.setPadding(new Insets(10));
         pntPrincipal.setBackground(new Background(fondo));
         pntPrincipal.setStyle("-fx-padding: 10; -fx-border-color: mediumblue; -fx-border-width: 2px");
+        Scene scene=new Scene(pntPrincipal, 640, 480);
+        primaryStage.setTitle("Eliminar Proveedor");
+        primaryStage.setScene(scene);
+        primaryStage.setMaxHeight(480);
+        primaryStage.setMinHeight(480);
+        primaryStage.setMaxWidth(640);
+        primaryStage.setMaxWidth(640);
+        primaryStage.show();
     }
-
-    public static void main(String[] args) {
-        launch(args);
-    }
-
+    
     public void btnLimpiarEventHandler(ActionEvent event) {
         ruc.setText("");
     }
 
-    public void btnIngresarEventHandler(ActionEvent event) {
+    public void btnEliminarEventHandler(ActionEvent event) {
     }
-
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
