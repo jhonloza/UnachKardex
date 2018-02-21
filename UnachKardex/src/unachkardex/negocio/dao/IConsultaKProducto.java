809684bd4db3ae910ac2d7ddceced9246b7cbd62
stian <stian@DESKTOP-T7F4CVQ>
2018-02-21 00:27:21 -0500

    consulta

--- UnachKardex/src/unachkardex/negocio/dao/IConsultaKProducto.java
+++ UnachKardex/src/unachkardex/negocio/dao/IConsultaKProducto.java
@@ -1,10 +1,10 @@
 
 package unachkardex.negocio.dao;
-
+import unachkardex.negocio.impl.*;
 import unachkardex.negocio.entidades.*;
 
 import java.util.ArrayList;
 public interface IConsultaKProducto {
-     public ArrayList<String[]> obtener() throws Exception;
+     public ArrayList<Kardex> obtener(int codProducto) throws Exception;
     
 }
