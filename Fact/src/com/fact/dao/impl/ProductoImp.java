ec2c2959147258756f72d02ae72f7bb548a958b1
Angel-Pc <Angel-Pc@192.168.10.119>
2018-02-26 17:15:34 -0500

    producto

--- Fact/src/com/fact/dao/impl/ProductoImp.java
+++ Fact/src/com/fact/dao/impl/ProductoImp.java
@@ -17,27 +17,23 @@ public class ProductoImp implements IProducto {
     public int insertar(Producto producto) {
         int numFilasAfectadas = 0;
         String sql = "INSERT INTO public.producto(\n"
-                + "            codigo_producto, nombre, color, marca, \"tamaño\", aroma, fecha_caducacion, \n"
+                + "            codigo_producto, nombre, fecha_caducacion, \n"
                 + "            categoria, precio_venta, precio_venta_cantidad, stock_cantidad, \n"
                 + "            stock, precio_total_venta, detalle)\n"
-                + "    VALUES (?, ?, ?, ?, ?, ?, ?, \n"
+                + "    VALUES (?, ?, ?, \n"
                 + "            ?, ?, ?, ?, \n"
                 + "            ?, ?, ?);";
         List<Parametro> lstPar = new ArrayList<>();
         lstPar.add(new Parametro(1, producto.getCodigo()));
         lstPar.add(new Parametro(2, producto.getNombre()));
-        lstPar.add(new Parametro(3, producto.getColor()));
-        lstPar.add(new Parametro(4, producto.getMarca()));
-        lstPar.add(new Parametro(5, producto.getTamaño()));
-        lstPar.add(new Parametro(6, producto.getAroma()));
-        lstPar.add(new Parametro(7, producto.getFechacaducacion()));
-        lstPar.add(new Parametro(8, producto.getCategoria().getCodigo()));
-        lstPar.add(new Parametro(9, producto.getPrecioventa()));
-        lstPar.add(new Parametro(10, producto.getPrecioventacantidad()));
-        lstPar.add(new Parametro(11, producto.getStockcantidad()));
-        lstPar.add(new Parametro(12, producto.getStock()));
-        lstPar.add(new Parametro(13, producto.getPreciototalventa()));
-        lstPar.add(new Parametro(14, producto.getDetalle()));
+        lstPar.add(new Parametro(3, producto.getFechacaducacion()));
+        lstPar.add(new Parametro(4, producto.getCategoria().getCodigo()));
+        lstPar.add(new Parametro(5, producto.getPrecioventa()));
+        lstPar.add(new Parametro(6, producto.getPrecioventacantidad()));
+        lstPar.add(new Parametro(7, producto.getStockcantidad()));
+        lstPar.add(new Parametro(8, producto.getStock()));
+        lstPar.add(new Parametro(9, producto.getPreciototalventa()));
+        lstPar.add(new Parametro(10, producto.getDetalle()));
         Conexion con = new Conexion();
         try {
             numFilasAfectadas = con.ejecutaComando(sql, lstPar);
@@ -53,26 +49,21 @@ public class ProductoImp implements IProducto {
     public int modificar(Producto producto) {
         int modificado = 0;
         String sql = "UPDATE public.producto\n"
-                + "   SET nombre=?, color=?, marca=?, \"tamaño\"=?, aroma=?, \n"
-                + "       fecha_caducacion=?, categoria=?, precio_venta=?, \n"
+                + "   SET nombre=?, fecha_caducacion=?, categoria=?, precio_venta=?, \n"
                 + "       precio_venta_cantidad=?, stock_cantidad=?, stock=?, \n"
                 + "       precio_total_venta=?, detalle=?\n"
                 + " WHERE codigo_producto = ?";
         List<Parametro> prts = new ArrayList<>();
         prts.add(new Parametro(1, producto.getNombre()));
-        prts.add(new Parametro(2, producto.getColor()));
-        prts.add(new Parametro(3, producto.getMarca()));
-        prts.add(new Parametro(4, producto.getTamaño()));
-        prts.add(new Parametro(5, producto.getAroma()));
-        prts.add(new Parametro(6, producto.getFechacaducacion()));
-        prts.add(new Parametro(7, producto.getCategoria().getCodigo()));
-        prts.add(new Parametro(8, producto.getPrecioventa()));
-        prts.add(new Parametro(9, producto.getPrecioventacantidad()));
-        prts.add(new Parametro(10, producto.getStockcantidad()));
-        prts.add(new Parametro(11, producto.getStock()));
-        prts.add(new Parametro(12, producto.getPreciototalventa()));
-        prts.add(new Parametro(13, producto.getDetalle()));
-        prts.add(new Parametro(14, producto.getCodigo()));
+        prts.add(new Parametro(2, producto.getFechacaducacion()));
+        prts.add(new Parametro(3, producto.getCategoria().getCodigo()));
+        prts.add(new Parametro(4, producto.getPrecioventa()));
+        prts.add(new Parametro(5, producto.getPrecioventacantidad()));
+        prts.add(new Parametro(6, producto.getStockcantidad()));
+        prts.add(new Parametro(7, producto.getStock()));
+        prts.add(new Parametro(8, producto.getPreciototalventa()));
+        prts.add(new Parametro(9, producto.getDetalle()));
+        prts.add(new Parametro(10, producto.getCodigo()));
         Conexion con = new Conexion();
         try {
             modificado = con.ejecutaComando(sql, prts);
@@ -105,7 +96,7 @@ public class ProductoImp implements IProducto {
     public Producto obtener(int id) throws Exception {
         Producto pro = null;
         ICategoria sqlCategoria = new CategoriaImp();
-        String sql = "SELECT codigo_producto, nombre, color, marca, \"tamaño\", aroma, fecha_caducacion, \n"
+        String sql = "SELECT codigo_producto, nombre, fecha_caducacion, \n"
                 + "       categoria, precio_venta, precio_venta_cantidad, stock_cantidad, \n"
                 + "       stock, precio_total_venta, detalle\n"
                 + "  FROM public.producto where codigo_producto=?";
@@ -118,18 +109,14 @@ public class ProductoImp implements IProducto {
                 pro = new Producto();
                 pro.setCodigo(rst.getInt(1));
                 pro.setNombre(rst.getString(2));
-                pro.setColor(rst.getString(3));
-                pro.setMarca(rst.getString(4));
-                pro.setTamaño(rst.getDouble(5));
-                pro.setAroma(rst.getString(6));
-                pro.setFechacaducacion(rst.getDate(7));
-                pro.setCategoria(sqlCategoria.obtener(rst.getInt(8)));
-                pro.setPrecioventa(rst.getDouble(9));
-                pro.setPrecioventacantidad(rst.getDouble(10));
-                pro.setStockcantidad(rst.getDouble(11));
-                pro.setStock(rst.getDouble(12));
-                pro.setPreciototalventa(rst.getDouble(13));
-                pro.setDetalle(rst.getString(14));
+                pro.setFechacaducacion(rst.getDate(3));
+                pro.setCategoria(sqlCategoria.obtener(rst.getInt(4)));
+                pro.setPrecioventa(rst.getDouble(5));
+                pro.setPrecioventacantidad(rst.getDouble(6));
+                pro.setStockcantidad(rst.getDouble(7));
+                pro.setStock(rst.getDouble(8));
+                pro.setPreciototalventa(rst.getDouble(9));
+                pro.setDetalle(rst.getString(10));
             }
         } catch (Exception e) {
             throw e;
@@ -143,7 +130,7 @@ public class ProductoImp implements IProducto {
     public List<Producto> obtener() throws Exception {
         List<Producto> lista = new ArrayList<>();
         ICategoria sqlCategoria = new CategoriaImp();
-        String sql = "SELECT codigo_producto, nombre, color, marca, \"tamaño\", aroma, fecha_caducacion, \n"
+        String sql = "SELECT codigo_producto, nombre, fecha_caducacion, \n"
                 + "       categoria, precio_venta, precio_venta_cantidad, \n"
                 + "       stock_cantidad, stock, precio_total_venta, \n"
                 + "       detalle\n"
@@ -155,18 +142,14 @@ public class ProductoImp implements IProducto {
                 Producto pro = new Producto();
                 pro.setCodigo(rst.getInt(1));
                 pro.setNombre(rst.getString(2));
-                pro.setColor(rst.getString(3));
-                pro.setMarca(rst.getString(4));
-                pro.setTamaño(rst.getDouble(5));
-                pro.setAroma(rst.getString(6));
-                pro.setFechacaducacion(rst.getDate(7));
-                pro.setCategoria(sqlCategoria.obtener(rst.getInt(8)));
-                pro.setPrecioventa(rst.getDouble(9));
-                pro.setPrecioventacantidad(rst.getDouble(10));
-                pro.setStockcantidad(rst.getDouble(11));
-                pro.setStock(rst.getDouble(12));
-                pro.setPreciototalventa(rst.getDouble(13));
-                pro.setDetalle(rst.getString(14));
+                pro.setFechacaducacion(rst.getDate(3));
+                pro.setCategoria(sqlCategoria.obtener(rst.getInt(4)));
+                pro.setPrecioventa(rst.getDouble(5));
+                pro.setPrecioventacantidad(rst.getDouble(6));
+                pro.setStockcantidad(rst.getDouble(7));
+                pro.setStock(rst.getDouble(8));
+                pro.setPreciototalventa(rst.getDouble(9));
+                pro.setDetalle(rst.getString(10));
                 lista.add(pro);
             }
         } catch (Exception e) {
