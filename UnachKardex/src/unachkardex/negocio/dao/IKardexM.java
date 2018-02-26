a056bbd458fc74054a66e38148d83c86cd503562
alewolf17 <35311177+alewolf17@users.noreply.github.com>
2018-02-25 23:25:34 -0500

    Add files via upload

---
+++ UnachKardex/src/unachkardex/negocio/dao/IKardexM.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package g3_subversion2.rnegocio.dao;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Wen
+ */
+public interface IKardexM {
+    //public ArrayList<Kardex> listadoKardexFecha(int codigoFacturaCompra) throws Exception;
+}
